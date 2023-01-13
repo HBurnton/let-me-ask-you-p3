cb782fb0e5d1f17d71c8bab4ae8b34db15a5f3b8
jschuyl <schuylertjed@gmail.com>
2023-01-12 19:44:30 -0700

    filling out the models a bit more

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,33 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
+const { Schema, model } = mongoose;
 const bcrypt = require('bcrypt');
+const Question = require('./Question');
+const Answer = required('./Answer')
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    questions: [Question],
+    answers: [Answer]
+});
+
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
