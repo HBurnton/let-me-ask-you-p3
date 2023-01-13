ceb23c667e2ec0615cc221b399626e14cf78cb16
jschuyl <schuylertjed@gmail.com>
2023-01-12 21:42:09 -0700

    got some more type defs in to make things visible in apollo

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,9 +5,33 @@ const typeDefs = gql`
         _id:ID
         name: String
     }
+    type Question{
+        _id: ID
+        questionText: String
+        category: Category
+        author: User
+        voteCount: Int
+        answers: [Answer]
+    }
+    type Answer{
+        _id: ID
+        answerText: String
+        author: User
+        question: Question
+    }
+    type User{
+        _id: ID
+        username: String
+        email: String
+        questions: [Question]
+        answers: [Answer]
+    }
 
     type Query {
         categories: [Category]
+        users: [User]
+        questions: [Question]
+        answers: [Answer]
     }
 `;
 
