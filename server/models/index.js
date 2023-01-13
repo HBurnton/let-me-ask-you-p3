6bbb6db65f376b8cd1b24b209a0f9d14bf5cc550
jschuyl <schuylertjed@gmail.com>
2023-01-12 19:18:54 -0700

    server model skeletons

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Question = require('./Question');
+const Category = require('./Category');
+const Answer = require('./Answer');
+
+module.exports = { User, Question, Category, Answer }
