30fd21d1108856c82b13b79bf0fc29fc8a82eb17
jschuyl <schuylertjed@gmail.com>
2023-01-18 21:17:08 -0700

    started seed loops, added answer seed

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,7 +1,8 @@
 const db = require('../config/connection');
 const { User, Category, Question, Answer } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const ca
+const categorySeeds = require('./categorySeeds.json');
+const questionSeeds = require('./questionSeeds.json')
 
 db.once('open', async () => {
     try {
@@ -11,12 +12,26 @@ db.once('open', async () => {
         await Question.deleteMany({});
         await Answer.deleteMany({});
 
-        // seed
+        // seed users and categories
         await User.create(userSeeds);
+        await Category.create(categorySeeds);
 
-        console.log('seeds are strewn');
-        process.exit(0)
+        for (let i=0; i < questionSeeds,length; i++) {
+            const { _id, author } = await Question.create(questionSeeds[i]);
+            const user = await User.findOneAndUpdate(
+                { username: author },
+                {
+                    $addToSet: {
+                        questions: _id,
+                    }
+                }
+            )
+        }
     } catch (err) {
-        throw err
+        console.err(err);
+        process.exit(1)
     }
+
+    console.log('seeds are strewn');
+    process.exit(0)
 })
