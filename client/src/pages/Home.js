5cfb3562c3dbb6d51073d392b6e9e6edc92adefb
hburnton <hburnton@gmail.com>
2023-01-26 07:23:22 -0700

    add base home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,42 +1,88 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { useQuery } from '@apollo/client';
 import QuestionList from '../components/QuestionList';
 import QuestionForm from '../components/QuestionForm';
 import styled from 'styled-components';
 import { Box, useMediaQuery } from "@mui/material";
 import '../assets/css/Home.css';
+import { useNavigate } from 'react-router-dom';
+import Auth from '../utils/auth'
 
 import { QUERY_QUESTIONS } from '../utils/queries';
 
 const Home = () => {
 
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const navigate = useNavigate();
+  const goToLogin = useCallback(() => navigate('/', {replace: true}), [navigate]);
+  const goToSignUp = useCallback(() => navigate('/signup', {replace: true}), [navigate]);
   
   const { loading, data } = useQuery(QUERY_QUESTIONS);
   const questions = data?.questions || [];
 
-  return (
-    <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <QuestionForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <QuestionList
-              questions={questions}
-              title="Some Feed for Question(s)..."
-            />
-          )}
-        </div>
+  if (Auth.loggedIn()) {
+    return (    
+      <div>
+      <Box>
+        <Box
+            width="100%"
+            padding="2rem 6%"
+            display={isNonMobileScreens ? "flex" : "block"}
+            gap="0.5rem"
+            justifyContent="center"
+            sx={{border: "white"}}
+          >
+            <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+            </Box>
+            <Box
+            flexBasis={isNonMobileScreens ? "42%" : undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+            > 
+            </Box>
+            {isNonMobileScreens && (
+            <Box flexBasis="26%">
+              <Box m="2rem 0" />
+            </Box>
+            )}
+          </Box>
+      </Box>
       </div>
-    </main>
-  );
-};
+    )
+  } else {
+    return <>
+    <LoginWarning>
+    <p>You must be logged in to view this page.</p> 
+    <br />
+    <p>Please do one of the following to view this content:</p>
+    <ButtonContainer>
+      <button className="youShallNotPass" onClick={goToLogin}>LOGIN</button>
+      <br />
+      <button className="youShallNotPass" onClick={goToSignUp}>SIGNUP</button>
+    </ButtonContainer>
+    </LoginWarning>
+    </>
+  }}
+  
+  
+  const ButtonContainer = styled.div`
+    margin: 1rem 0 2rem 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `;
+  
+  const LoginWarning = styled.div`
+   display : flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   margin: 25vh;
+  `
+  
+  
+  
 
 export default Home;
 
