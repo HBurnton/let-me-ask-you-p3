e532874f1867e26826ae534896ceac190a67935b
Quantum <autumnlegere@gmail.com>
2023-01-25 21:13:19 -0700

    add and display comments

--- client/src/components/QuestionForm/index.js
+++ client/src/components/QuestionForm/index.js
@@ -70,3 +70,38 @@ const MainContainer = styled.div`
 `;
 
 export default QuestionForm;
+
+  // ORIGINAL CODE FOR ADDCOMMENT
+
+  // export const AddComment = () => {
+  //   return (
+  //   <WidgetWrapper>
+  //       <FlexBetween gap="1.5rem">
+  //         <UserImage />
+  //         <InputBase
+  //           placeholder="Comment on this..."
+  //           sx={{
+  //             width: "100%",
+  //             backgroundColor: 'white',
+  //             borderRadius: "2rem",
+  //             padding: "1rem 2rem",
+  //           }}
+  //         />
+  //           <FlexBetween>
+  //           <Button
+  //               sx={{
+  //               color: "white",
+  //               backgroundColor: "blue",
+  //               borderRadius: "3rem",
+  //               }}
+  //           >
+  //               SEND
+  //           </Button>
+  //           </FlexBetween>
+  //           </FlexBetween>
+
+  //       <Divider sx={{ margin: "1.25rem 0" }} />
+  
+  //     </WidgetWrapper>
+  //   );
+  // };
